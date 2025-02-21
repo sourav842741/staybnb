caab9daf24b9606d3ab1f14055201d508128ce01
Sourav kumar <161188554+sourav842741@users.noreply.github.com>
2025-02-21 17:12:06 +0530

    Update schema.js

--- schema.js
+++ schema.js
@@ -14,6 +14,6 @@ module.exports.listingSchema = joi.object({
 module.exports.reviewSchema = joi.object({
     review:joi.object({
         rating:joi.number().required().min(1).max(5),
-        comment:joi.string().required,
+        comment:joi.string().required(),
     }).required()
 })
